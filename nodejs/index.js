87e9a0d56176593ca20e257669bdc8e3d72defd9
Even Torbjørnsen <591118@stud.hvl.no>
2023-11-05 17:05:18 +0100

    ferdig oblig 7

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const path = require('path');
+const http = require('http'); // Require http module for server
+const WebSocket = require('ws');
+
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, )));
+const server = http.createServer(app); // Create an HTTP server using Express app
+const wss = new WebSocket.Server({ server }); // Attach WebSocket server to the HTTP server
 
-let count = 0;
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 11); // Generates a number from 0 to 10
+}
+
+wss.on('connection', ws => {
+    let count = 0;
+    const interval = setInterval(() => {
+            const newData = {
+                x: generateRandomNumber(),
+                y: count
+            };
+            ws.send(JSON.stringify(newData));
+            count++;
+       
+    }, 1000);
+});
+
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
