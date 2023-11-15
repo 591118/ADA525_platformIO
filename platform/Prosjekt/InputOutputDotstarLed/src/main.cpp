eacc9126f7ff6312b8ec9389675842549cc80dda
Even Torbjørnsen <591118@stud.hvl.no>
2023-11-15 11:33:19 +0100

    Vi he lys

--- platform/Prosjekt/InputOutputDotstarLed/src/main.cpp
+++ platform/Prosjekt/InputOutputDotstarLed/src/main.cpp
@@ -2,42 +2,30 @@
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
 #include <Adafruit_SHT31.h>
-#include <MPU6050.h>
+#include <Adafruit_LSM6DS33.h>
 
 // Everything is defined in the Board Support Package
 // DOTSTAR_NUM        number of onboard DotStars (typically just 1)
 // PIN_DOTSTAR_DATA   onboard DotStar data pin
 // PIN_DOTSTAR_CLK    onboard DotStar clock pin
-Adafruit_DotStar strip(6, 6, 9, DOTSTAR_BRG);
+Adafruit_DotStar strip(6, 6, 5, DOTSTAR_BRG);
 Adafruit_BMP280 bmp280;     // temperautre, barometric pressure
 Adafruit_SHT31 sht30;       // humidity
-Adafruit_MPU6050 mpu;       //gyro
+Adafruit_LSM6DS33 lsm;    // gyro
 
-int ledPin = 6;
-int oppvekkingsDelayIntervall = 0;   // 8 sekunder intervall  - 8000
-int antallLeds = 6;                     // antall ledlys
-int brightness;
+int antallLeds = 6;
+int brightness = 0;
 bool heltPaaskrudd = false;
-uint16_t hue;
 float temperature, pressure, altitude, humidity;
-int16_t gyroX, gyroY, gyroZ;
-
+float ax, ay, az, gx, gy, gz;
 
 void setup() {
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
-  brightness = 0;
   bmp280.begin();
   sht30.begin();
-
+  lsm.begin_I2C();
   Serial.begin(115200);
-
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
 }
 
 void sun () { 
@@ -54,40 +42,23 @@ void loop() {
 
   humidity = sht30.readHumidity();
 
-  mpu.getRotation(&gyroX, &gyroY, &gyroZ);
-
   Serial.println("Brightness: " + String(brightness));
   Serial.println("temp: " + String(temperature));
   Serial.println("altitude: " + String(altitude));
   Serial.println("humidity: " + String(humidity));
-  Serial.println("GyroX: " + String(gyroX));
-  Serial.println("GyroY: " + String(gyroY));
-  Serial.println("GyroZ: " + String(gyroZ));
 
-    if (temperature > 24.00 && altitude > 187.00){
-      strip.clear(); 
-      strip.setBrightness(10);
-      strip.setPixelColor(i,0,255,0);
-      strip.show();
-      delay(100);
+ if (true) {
+    strip.clear();
+    strip.setBrightness(brightness);
+
+    for (int i = 0; i < antallLeds; i++) {
+      strip.setPixelColor(i, 0, 255, 0);
+      strip.setBrightness(64);
     }
+
+    strip.show();
+    delay(100);
+  }
   
 delay(1000);
-
- // if (heltPaaskrudd == false){
-  //     sun (); 
-  // } else {
-  //   hue = 23593;    //(120 * 65535) / 360 = 23593
-  // }
-  //   Serial.println("Brightness: " + String(brightness));
-  //   hue = (249309 + brightness) % 65536;
-  //   strip.fill(strip.ColorHSV(hue, 29 * 255 / 100, brightness));
-  //   strip.show();
-  //   delay(7000);
-
   }
-
- 
-
-    //  rainbow(10);  // Flowing rainbow cycle along the whole stri
-  
